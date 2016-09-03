f7917cc75efaa21783596f0d5569a9702a19b64d
Nathanael Mann <nathanael.mann@cit-design.co.uk>
2016-09-03 20:42:29 +0100

    Removed some unnecessary files

---
+++ Sitecore.Boost/Sitecore.Boost/InitializeProfiling.cs
@@ -0,0 +1,19 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Mvc.Pipelines.Response.RenderRendering;
+
+namespace Sitecore.Boost.InitializeProfiling
+{
+    public class InitializeProfiling : Sitecore.Mvc.Pipelines.Response.RenderRendering.InitializeProfiling
+    {
+        protected override void StartProfiling(RenderRenderingArgs args)
+        {
+            // This line is heavy
+            // string str = args.Rendering.ToString();
+            // Replaced with this as an example
+            string str = args.Rendering.Id.ToString();
+            Tracer.Info("Starting rendering \"" + str + "\".");
+            ++Tracer.Indent;
+            Profiler.StartOperation("Render \"" + str + "\".");
+        }
+    }
+}
